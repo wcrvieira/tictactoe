3a6292cc6f8dcc02a088db6bec90de6985ab590e
wcrvieira <wagner.vieira@etec.sp.gov.br>
2023-10-01 20:23:18 -0300

    Iniciando o projeto

---
+++ js/main.js
@@ -0,0 +1,16 @@
+	letra = "X";
+ 
+	function joga(celula){
+		let celulaclicada = document.getElementById(celula).innerHTML;
+		if (celulaclicada == "X" || celulaclicada == "O"){
+			alert("Opa, este quadrado já foi escolhido!");
+		}else{
+			document.getElementById(celula).innerHTML = letra;
+			if (letra == "X"){
+				letra = "O";
+			}else{
+				letra = "X";
+			}
+		}
+	}
+ 
